49f845bd65a843548e2dad4996520da21303505b
wongandydev <wongandy97@gmail.com>
2020-05-09 11:25:39 -0400

    solve 100 in 5 minutes

---
+++ Algorithms/100_same_tree.swift
@@ -0,0 +1,13 @@
+func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+    if p?.val != q?.val {
+        return false
+    } 
+    
+    if p == nil && q == nil {
+        return true 
+    } else if p == nil || q == nil {
+        return false 
+    }
+    
+    return isSameTree(p!.left, q!.left) && isSameTree(p!.right, q!.right)
+}
